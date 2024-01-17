15871e3796d1ad370ad8dd5fd1194fe7af0a32ab
Aidan Loughran <aidan@loughran.dev>
2024-01-17 01:56:09 +0000

    feat: derived signal & a refactor

---
+++ packages/js/src/signal/function/create-computed.ts
@@ -0,0 +1,47 @@
+import { queueUniqueMicrotask } from '../../microtask/function/queue-unique-microtask';
+import { createSignal } from '../function/create-signal';
+import { toReadOnlySignal } from '../function/to-read-only-signal';
+import type { ReadableSignal } from '../types';
+
+type InferredArgs<T extends ReadableSignal<any>[]> = {
+  [K in keyof T]: ReturnType<T[K] extends ReadableSignal<infer U> ? ReadableSignal<U> : never>;
+};
+
+/**
+ * Creates a derived signal, which is a special `ReadableSignal` where the value is automatically
+ * derived from other signal(s).
+ *
+ * @param dependencies An array of the signal(s) needed to compute the value. Whenever any one of
+ *   these signals push a new value, this signal will re-compute and push a new value too. Note that
+ *   you may even use another derived signal as a dependency, however exercise caution as
+ *   introducing circular dependencies will freeze your program because there is not yet any
+ *   mechnanism to detect them (TODO!)
+ * @param callback A callback that accepts a mapped version of the values of the signals given in
+ *   `dependencies` array. The returned value is the value is the new value of the derived signal.
+ *   If your signal depends on multiple other signals, you will need to manually narrow the type.
+ *   This is because the inferred value type becomes a union of all possible values (TODO!)
+ */
+export const createDerived = <TOutput, TInputs extends ReadableSignal<any>[]>(
+  dependencies: TInputs,
+  callback: (...args: InferredArgs<TInputs>) => TOutput,
+): ReadableSignal<TOutput> => {
+  const signal = createSignal(callback(...(dependencies.map((d) => d()) as InferredArgs<TInputs>)));
+
+  const compute = () => {
+    const newValue = callback(...(dependencies.map((d) => d()) as InferredArgs<TInputs>));
+    if (signal() !== newValue) {
+      signal.set(newValue);
+    }
+  };
+
+  for (const signal of dependencies) {
+    signal.subscribe(() => queueUniqueMicrotask(compute));
+  }
+
+  return toReadOnlySignal(signal);
+
+  /**
+   * TODO: to make more efficient, while no one is listening, don't compute and unsubscribe from
+   * dependencies. Don't notify unless value actually changes.
+   */
+};
--- packages/js/src/signal/function/create-signal.ts
+++ packages/js/src/signal/function/create-signal.ts
@@ -1,24 +1,25 @@
+import { queueUniqueMicrotask } from '../../microtask/function/queue-unique-microtask';
 import type { Subscriber, WritableSignal } from '../types';
 
-export const constructCreateSignal = (addToNotifyQueue: (item: () => void) => void) => {
-  return <T>(initialValue: T): WritableSignal<T> => {
-    let currentValue = initialValue;
-    const signal = () => currentValue;
-    const subscribers = new Set<Subscriber<T>>();
+export const createSignal = <T>(initialValue: T): WritableSignal<T> => {
+  let currentValue = initialValue;
+  const signal = () => currentValue;
+  const subscribers = new Set<Subscriber<T>>();
 
-    const push = () => subscribers.forEach((subscriber) => subscriber(currentValue));
+  const push = () => subscribers.forEach((subscriber) => subscriber(currentValue));
 
-    signal.set = (newValue: T) => {
-      currentValue = newValue;
-      addToNotifyQueue(push);
-    };
-
-    signal.subscribe = (subscriber: Subscriber<T>) => {
-      subscribers.add(subscriber);
-      subscriber(currentValue);
-      return () => subscribers.delete(subscriber);
-    };
-
-    return signal;
+  signal.set = (newValue: T) => {
+    currentValue = newValue;
+    // TODO: to make even more efficient: notify computed stores first,
+    // have them calculate, then notify all subscribers in one pass
+    queueUniqueMicrotask(push);
   };
+
+  signal.subscribe = (subscriber: Subscriber<T>) => {
+    subscribers.add(subscriber);
+    subscriber(currentValue);
+    return () => subscribers.delete(subscriber);
+  };
+
+  return signal;
 };
