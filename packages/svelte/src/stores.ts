324ab38578a11eb4a91c56c4dee2e0acd9e35585
Aidan Loughran <aidan@loughran.dev>
2024-01-27 18:04:56 +0000

    refactor(svelte): use `signalToStore`

--- packages/svelte/src/stores.ts
+++ packages/svelte/src/stores.ts
@@ -1,25 +1,18 @@
-import { createEffect, type SignalGetter } from '@adamantjs/signals';
+import { type SignalGetter, signalToStore } from '@adamantjs/signals/svelte';
 import { session } from 'librebase';
-import { readable } from 'svelte/store';
+import { readable, type Readable } from 'svelte/store';
 
 // TODO: lazy create stores by appID, cache and re-return them
 //       maybe just use createModule from core package
 
-export function signalToStore<T>(signal: SignalGetter<T>) {
-  // https://svelte.dev/docs/svelte-store
-  // "[...] called when the number of subscribers goes from zero to one [...]"
-  // "[...] return a stop function that is called when the subscriber count goes from one to zero."
-  return readable<T>(signal(), (update) => {
-    return createEffect(() => update(signal()));
-  });
+function createStore<T>(signal: SignalGetter<T>): Readable<T> {
+  return window.globalThis ? signalToStore(signal) : readable();
 }
 
 export function activeSession<T>(appID?: string) {
-  if (!window.globalThis) return readable();
-  return signalToStore(session<T>(appID).activeSession);
+  return createStore(session<T>(appID).activeSession);
 }
 
 export function allSessions<T>(appID?: string) {
-  if (!window.globalThis) return readable();
-  return signalToStore(session<T>(appID).allSessions);
+  return createStore(session<T>(appID).allSessions);
 }
