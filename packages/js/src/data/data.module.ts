216d48b8fa7dc6d1a25e8ffbf8f3157152a4bf5b
Aidan Loughran <aidan@loughran.dev>
2024-02-20 21:51:44 +0000

    refactor(data): add `DataModule` type

--- packages/js/src/data/data.module.ts
+++ packages/js/src/data/data.module.ts
@@ -1,12 +1,19 @@
 import { getChannels } from '../channel/channel.module';
 import type { ChannelDriver } from '../channel/types';
 import { createModule } from '../module/create-module';
-import { createNode, getNode } from './node';
+import { createNode, getNode, type Node } from './node';
 import { JsonSerializer, TextSerializer, type Serializer } from './serializer';
 
 export type Serializers = Record<string, Serializer<unknown>>;
 
-export const getDataModule = createModule((key) => {
+export interface DataModule {
+  createNode(): Node;
+  getNode(hash: Uint8Array): Promise<Node | void | undefined>;
+  registerChannelDriver(driver: ChannelDriver): void;
+  registerSerializer<T>(mediaType: string, serializer: Serializer<T>): void;
+}
+
+export const getDataModule = createModule<DataModule>((key) => {
   const channels = getChannels(key);
   const serializers: Serializers = {
     'application/json': JsonSerializer,
