a171d2a05da065e9510afb8737f4193e9c09a126
Aidan Loughran <aidan@loughran.dev>
2024-03-02 20:56:29 +0000

    feat(svelte): add identity store getter

--- packages/svelte/src/stores.ts
+++ packages/svelte/src/stores.ts
@@ -1,6 +1,6 @@
 import { type SignalGetter, signalToStore } from '@adamantjs/signals/svelte';
 import { keyring, type ActiveKeyring } from 'librebase';
-import { readable, type Readable } from 'svelte/store';
+import { derived, readable, type Readable } from 'svelte/store';
 
 // TODO: lazy create stores by appID, cache and re-return them
 //       maybe just use createModule from core package
@@ -9,6 +9,12 @@ function createStore<T>(signal: SignalGetter<T>): Readable<T> {
   return window.globalThis ? signalToStore(signal) : readable();
 }
 
-export function activeSession<T>(appID?: string) {
-  return createStore<ActiveKeyring<T> | undefined>(keyring(appID).active);
+export function activeKeyring<T>(instanceID?: string) {
+  return createStore<ActiveKeyring<T> | undefined>(keyring(instanceID).active);
+}
+
+export function identity(identityID: string, instanceID?: string) {
+  return derived(activeKeyring(instanceID), (keyring) =>
+    Promise.resolve(keyring?.getIdentity(identityID)),
+  );
 }
