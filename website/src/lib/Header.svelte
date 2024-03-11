24a325507c8bde2cd63f842e04200d52d4b63aad
Aidan Loughran <aidan@loughran.dev>
2024-03-11 23:33:47 +0000

    docs: update website design

---
+++ website/src/lib/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+  import SocialButtons from './SocialButtons.svelte';
+</script>
+
+<header>
+  <a class="logo" href="/">Librebase</a>
+  <SocialButtons />
+</header>
+
+<style>
+  header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: sticky;
+  }
+
+  .logo {
+    font-size: 24px;
+    text-decoration: none;
+    font-weight: 900;
+    margin-right: 96px;
+  }
+
+  a {
+    color: var(--txt);
+  }
+</style>
