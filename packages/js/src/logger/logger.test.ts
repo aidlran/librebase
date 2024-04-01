d1a1b1337160ccd09a7ce8680aa95f3077a89f1f
Aidan Loughran <aidan@loughran.dev>
2024-04-01 17:00:28 +0100

    test: update logger tests

--- packages/js/src/logger/logger.test.ts
+++ packages/js/src/logger/logger.test.ts
@@ -1,19 +1,19 @@
 import { describe, vi, it, expect } from 'vitest';
-import { error, log, setLoggingEnabled, warn } from './logger';
+import { error, log, setLogLevel, warn } from './logger';
 
 describe('Logger', () => {
   for (const fn of [error, log, warn]) {
     describe(fn.name, () => {
-      it('Logs when logging enabled', () => {
+      it('Logs when log level set to all', () => {
         const spy = vi.spyOn(console, fn.name as keyof Console);
-        setLoggingEnabled(true);
-        fn('Logging while enabled test!');
+        setLogLevel('all');
+        fn({}, 'Logging while enabled test!');
         expect(spy).toHaveBeenCalled();
       });
-      it('Does not log when logging disabled', () => {
+      it('Does not log when log level set to none', () => {
         const spy = vi.spyOn(console, fn.name as keyof Console);
-        setLoggingEnabled(false);
-        fn('Logging while disabled test!');
+        setLogLevel('none');
+        fn({}, 'Logging while disabled test!');
         expect(spy).not.toHaveBeenCalled();
       });
     });
