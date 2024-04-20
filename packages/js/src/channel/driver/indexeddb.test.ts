e6d3d0e8f2791600d2b0b475155a44273d4b23b7
Aidan Loughran <aidan@loughran.dev>
2024-04-20 16:12:46 +0100

    test: add indexeddb channel driver tests

--- packages/js/src/channel/driver/indexeddb.test.ts
+++ packages/js/src/channel/driver/indexeddb.test.ts
@@ -1,16 +1,28 @@
-import { describe, expect, it } from 'vitest';
 import 'fake-indexeddb/auto';
+import { describe, expect, test } from 'vitest';
 import { indexeddb } from './indexeddb';
 
-describe('IndexedDB channel driver', () => {
-  it('Constructs a channel interface', async () => {
-    const channel = await indexeddb();
-    expect(channel).toBeTypeOf('object');
-    expect(channel.deleteObject).toBeTypeOf('function');
-    expect(channel.getAddressHash).toBeTypeOf('function');
-    expect(channel.getObject).toBeTypeOf('function');
-    expect(channel.putObject).toBeTypeOf('function');
-    expect(channel.setAddressHash).toBeTypeOf('function');
-    expect(channel.unsetAddressHash).toBeTypeOf('function');
+describe('IndexedDB channel driver', async () => {
+  const channel = await indexeddb();
+  const hash = crypto.getRandomValues(new Uint8Array(16));
+
+  test('Object CRUD', async () => {
+    const object = crypto.getRandomValues(new Uint8Array(64));
+    await expect(channel.getObject(hash)).resolves.toBeUndefined();
+    await expect(channel.putObject(hash, object)).resolves.toBeUndefined();
+    await expect(channel.getObject(hash)).resolves.toEqual(object);
+    await expect(channel.deleteObject(hash)).resolves.toBeUndefined();
+    await expect(channel.getObject(hash)).resolves.toBeUndefined();
+  });
+
+  test('Address CRUD', async () => {
+    const address = crypto.getRandomValues(new Uint8Array(16));
+    await expect(channel.getAddressHash(address)).resolves.toBeUndefined();
+    await expect(channel.setAddressHash(address, hash)).resolves.toBeUndefined();
+    await expect(channel.getAddressHash(address)).resolves.toEqual(hash);
+    await expect(channel.unsetAddressHash(address)).resolves.toBeUndefined();
+    await expect(channel.getAddressHash(address)).resolves.toBeUndefined();
   });
 });
+
+test.todo('Multiple instances with different database or table names');
