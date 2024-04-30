0b0b15b3449f8731e8f990f6a1c7d205c4cdc4e0
Aidan Loughran <aidan@loughran.dev>
2024-04-30 04:39:17 +0100

    chore: update configs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
@@ -6,7 +6,11 @@ export default defineConfig({
     include: ['packages/**/*.{test,spec}.{js,ts}'],
     coverage: {
       include: ['packages/**/*.{js,ts}'],
-      exclude: ['**/index.{js,ts}'],
+      exclude: [
+        ...configDefaults.coverage.exclude,
+        '**/index.{js,ts}',
+        'packages/core/src/internal.ts',
+      ],
     },
   },
 });
