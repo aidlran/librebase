6be3970d76e0d8a367dd9441240d1c3f3f96f0b7
Aidan Loughran <aidan@loughran.dev>
2024-05-24 02:02:23 +0100

    docs: update typedoc config

--- packages/core/src/public/index.ts
+++ packages/core/src/public/index.ts
@@ -3,13 +3,3 @@ export * from './buffer-utils';
 export * from './channels';
 export * from './identifiers';
 export * from './operations';
-
-export { logLevels, setLogLevel, type LogLevel } from '../internal/logger';
-
-export type {
-  RegisterOptions,
-  Registry,
-  RegistryKey,
-  RegistryOptions,
-  RegistryValue,
-} from '../internal/registry';
